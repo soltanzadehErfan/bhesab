31e7204fe79a2c58024d57d634097306d9dd4c4d
wofaaa <soltanzadeh836@gmail.com>
2024-08-01 19:03:56 +0330

    develop

--- lib/services/theme_service.dart
+++ lib/services/theme_service.dart
@@ -4,17 +4,17 @@ import '../utils/safe_provider.dart';
 import '../theme/theme.dart';
 
 class ThemeNotifier extends SafeProvider {
-  bool _isDarkTheme = false;
+  bool _isLightTheme = false;
 
-  bool get isDarkTheme => _isDarkTheme;
+  bool get isDarkTheme => _isLightTheme;
 
-  ThemeData get currentTheme => _isDarkTheme ? darkTheme : lightTheme;
+  ThemeData get currentTheme => _isLightTheme ? lightTheme : darkTheme;
 
   IconData get themeIcon =>
-      _isDarkTheme ? Icons.light_mode_rounded : Icons.dark_mode_rounded;
+      _isLightTheme ? Icons.dark_mode_rounded : Icons.light_mode_rounded;
 
   void toggleTheme() {
-    _isDarkTheme = !_isDarkTheme;
+    _isLightTheme = !_isLightTheme;
     notifyListeners();
   }
 }
